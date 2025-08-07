190bed677e5eac98b99995323d7a349a09a9d373
Sidhartha Kasyap <kasyapsidhartha@gmail.com>
2025-08-08 00:09:10 +0530

    feat: add Navbar and ResumeCard components; define Resume and Feedback types

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,8 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/ResumeCard";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +12,35 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
-  </main>;
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your submission and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+
+        {resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link
+              to="/upload"
+              className="primary-button w-fit text-xl font-semibold"
+            >
+              Upload Resume
+            </Link>
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
