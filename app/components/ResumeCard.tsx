b8e416d2e751cc69a40b322914954e23b85b4401
Sidhartha HIBR <developer3@havelockislandbeachresort.com>
2025-09-04 16:34:50 +0530

    feat: enhance ResumeCard and Home components; implement resume loading logic and improve user feedback

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
   resume: Resume;
 }) => {
-    const [resumeUrl, setResumeUrl] = useState('');
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+
+    loadResume();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -25,22 +39,21 @@ const ResumeCard = ({
             <h2 className="!text-black font-bold">Resume</h2>
           )}
         </div>
-
         <div className="flex-shrink-0">
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
-      {/* {resumeUrl && ( */}
+      {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
             <img
-              src={imagePath}
+              src={resumeUrl}
               alt="resume"
               className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
             />
           </div>
         </div>
-      {/* )} */}
+      )}
     </Link>
   );
 };
